5f3b263325c0ba4ffb867c95f6d8fc15e3a806c0
Manu Sreedharan <manu.ts@outlook.com>
2026-06-24 07:52:30 +0200

    update perfetto sdk

--- third_party/perfetto/perfetto.cc
+++ third_party/perfetto/perfetto.cc
@@ -52205,8 +52205,8 @@ const char* GetVersionCode();
 #ifndef GEN_PERFETTO_VERSION_GEN_H_
 #define GEN_PERFETTO_VERSION_GEN_H_
 
-#define PERFETTO_VERSION_STRING() "v57.0-c075ca324"
-#define PERFETTO_VERSION_SCM_REVISION() "c075ca324bc11cefc071afa0f2229657353f76a3"
+#define PERFETTO_VERSION_STRING() "v57.0-4c388228e"
+#define PERFETTO_VERSION_SCM_REVISION() "4c388228e679c7dc1578d3d2b3bd7f750b3d4b0f"
 
 #endif  // GEN_PERFETTO_VERSION_GEN_H_
 /*
@@ -77055,6 +77055,63 @@ std::unique_ptr<LinuxFileWatch> WatchUnixSocketCreation(
 }  // namespace perfetto
 
 #endif  // INCLUDE_PERFETTO_EXT_BASE_UNIX_SOCKET_H_
+// gen_amalgamated begin header: src/base/vm_sockets.h
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_BASE_VM_SOCKETS_H_
+#define SRC_BASE_VM_SOCKETS_H_
+
+// gen_amalgamated expanded: #include "perfetto/base/build_config.h"
+
+#if PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX) || \
+    PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID)
+
+#include <sys/socket.h>
+
+#if PERFETTO_BUILDFLAG(PERFETTO_OS_QNX) && __has_include(<vm_sockets.h>)
+// QNX with Advanced Virtualization Framework
+#include <vm_sockets.h>
+#elif defined(AF_VSOCK)
+// Use system vm_socket.h if available.
+#include <linux/vm_sockets.h>
+#else  // defined(AF_SOCK)
+// Fallback and use the stripped copy from the UAPI vm_sockets.h.
+
+#include <stdint.h>  // For uint8_t.
+
+#define AF_VSOCK 40
+
+struct sockaddr_vm {
+  sa_family_t svm_family;
+  unsigned short svm_reserved1;
+  unsigned int svm_port;
+  unsigned int svm_cid;
+  uint8_t svm_flags;
+  unsigned char svm_zero[sizeof(struct sockaddr) - sizeof(sa_family_t) -
+                         sizeof(unsigned short) - sizeof(unsigned int) -
+                         sizeof(unsigned int) - sizeof(uint8_t)];
+};
+
+#endif  // defined(AF_SOCK)
+
+#endif  // PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX) ||
+        // PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID)
+
+#endif  // SRC_BASE_VM_SOCKETS_H_
 /*
  * Copyright (C) 2017 The Android Open Source Project
  *
